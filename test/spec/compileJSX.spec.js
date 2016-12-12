c3401e31a964efa3a60229b8880e2a034506bcce
Roman Stremedlovskyi <Roman_Stremedlovskyi@epam.com>
2016-12-12 20:00:35 +0200

    (feature) testing env

---
+++ test/spec/compileJSX.spec.js
@@ -0,0 +1,27 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import { compileJSX } from '../../src/compileJSX.js';
+
+chai.use(sinonChai);
+
+describe('Compile JSX', () => {
+    it('should represent DOM elements with JS objects', () => {
+        const expected = {
+            type: 'ul',
+            props: {class: 'list'},
+            children: [
+                {type: 'li', props: {}, children: ['item 1']},
+                {type: 'li', props: {}, children: ['item 2']}
+            ]
+        };
+        const actual = compileJSX(
+            'ul',
+            {class: 'list'},
+            compileJSX('li', {}, 'item 1'),
+            compileJSX('li', {}, 'item 2')
+        );
+        expect(expected).to.deep.equal(actual);
+    });
+});
